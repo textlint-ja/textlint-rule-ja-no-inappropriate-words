7b178b3672b63ba7a8b9d0772af9ebbc908d39fb
Tomoyuki Hata <hato6502@gmail.com>
2020-05-23 14:18:54 +0900

    Support dictionary cache

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,13 @@ import 'isomorphic-fetch';
 
 import { TextlintRuleModule } from '@textlint/types';
 import fastXmlParser from 'fast-xml-parser';
+const fs = require('fs');
+import os from 'os';
 
 const dictionaryUrl = 'http://monoroch.net/kinshi/housouKinshiYougo.xml';
 const referenceUrl = 'http://monoroch.net/kinshi/';
+const dictionaryPath = `${os.tmpdir()}/housouKinshiYougo.xml`;
+const maxAge = 604800;
 
 interface Dictionary {
   housouKinshiYougoList?: {
@@ -18,23 +22,32 @@ interface Dictionary {
   };
 }
 
-let cachedDictionary: Dictionary | undefined;
-
 const fetchDictionary = async () => {
-  if (cachedDictionary) {
-    return cachedDictionary;
-  }
-
   const response = await fetch(dictionaryUrl);
 
   if (response.status >= 400) {
     throw new Error(`${response.status}: ${response.statusText}`);
   }
 
-  const text = await response.text();
-  const dictionary: Dictionary = fastXmlParser.parse(text);
+  return response.text();
+}
 
-  cachedDictionary = dictionary
+const readDictionaryFromFs = () => {
+  try {
+    const stats = fs.statSync(dictionaryPath);
+
+    if (new Date().getTime() - stats.mtime.getTime() < maxAge * 1000) {
+      return fs.readFileSync(dictionaryPath).toString();
+    }
+  } catch { }
+}
+
+const getDictionary = async () => {
+  const text = readDictionaryFromFs() || await fetchDictionary();
+
+  fs.writeFileSync(dictionaryPath, text);
+
+  const dictionary: Dictionary = fastXmlParser.parse(text);
 
   return dictionary;
 }
@@ -46,7 +59,7 @@ const report: TextlintRuleModule = (context) => {
     async [Syntax.Str](node) {
       const text = getSource(node);
 
-      const { housouKinshiYougoList } = await fetchDictionary();
+      const { housouKinshiYougoList } = await getDictionary();
 
       housouKinshiYougoList?.dirtyWord?.forEach(({ notes, replaceWordList, word = '' }) => {
         const index = text.indexOf(word);
